e315e48c1d5969e90b65d77bd93139abf036a594
francis.belanger@ubisoft.com <francis.belanger@gmail.com>
2023-10-16 10:45:02 -0400

    feat(nvim): set GIT_EDITOR for git operations in nvim

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -11,83 +11,89 @@ vim.cmd("command! -nargs=?  Gri :R git rebase -i <args>")
 vim.cmd("command! Gtrack :R git push -u origin $(git symbolic-ref --short -q HEAD)")
 
 local rebase_interactive_hash = function()
-	local lib = require("diffview.lib")
-	local view = lib.get_current_view()
-	if view then
-		if view.panel:is_focused() then
-			local item = view.panel:get_item_at_cursor()
-			if item then
-				vim.cmd("Gri " .. item.commit.hash)
-				print(item.commit.hash)
-			end
-		end
-	end
+  local lib = require("diffview.lib")
+  local view = lib.get_current_view()
+  if view then
+    if view.panel:is_focused() then
+      local item = view.panel:get_item_at_cursor()
+      if item then
+        vim.cmd("Gri " .. item.commit.hash)
+        print(item.commit.hash)
+      end
+    end
+  end
 end
 
 return {
-	{ "tpope/vim-fugitive" },
-	-- {
-	--   "rbong/vim-flog",
-	--   cmd = { "Flog", "FlogSplit" },
-	--   keys = {
-	--     { "<leader>gfl", "<cmd>Flog<cr>", desc = { "Open Git log graph" } },
-	--   },
-	-- },
-	{
-		"lewis6991/gitsigns.nvim",
-		opts = {
-			signs = {
-				add = { text = "▐" },
-				change = { text = "▐" },
-				delete = { text = "" },
-				topdelete = { text = "" },
-				changedelete = { text = "▐" },
-				untracked = { text = "▐" },
-			},
-		},
-	},
-	{
-		"sindrets/diffview.nvim",
-		branch = "main",
-		cmd = { "DiffviewOpen", "DiffviewFileHistory" },
-		keys = {
-			{ "<leader>gd", "<cmd>DiffviewOpen<CR>", desc = "Git Diff View" },
-			{ "<leader>gfh", "<cmd>DiffviewFileHistory<CR>", desc = "Git Diff File History" },
-		},
-		opts = function()
-			local actions = require("diffview.actions")
+  { "tpope/vim-fugitive" },
+  -- {
+  --   "rbong/vim-flog",
+  --   cmd = { "Flog", "FlogSplit" },
+  --   keys = {
+  --     { "<leader>gfl", "<cmd>Flog<cr>", desc = { "Open Git log graph" } },
+  --   },
+  -- },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signs = {
+        add = { text = "▐" },
+        change = { text = "▐" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▐" },
+        untracked = { text = "▐" },
+      },
+    },
+  },
+  {
+    "sindrets/diffview.nvim",
+    branch = "main",
+    cmd = { "DiffviewOpen", "DiffviewFileHistory" },
+    keys = {
+      { "<leader>gd", "<cmd>DiffviewOpen<CR>", desc = "Git Diff View" },
+      { "<leader>gfh", "<cmd>DiffviewFileHistory<CR>", desc = "Git Diff File History" },
+    },
+    opts = function()
+      if vim.fn.executable("nvr") == 1 then
+        local nvr = "nvr --servername " .. vim.v.servername .. " "
+        vim.env.GIT_EDITOR = nvr .. " +'setl bh=delete' --remote-wait"
+        vim.env.EDITOR = nvr .. "-l --remote" -- (Optional)
+        vim.env.VISUAL = nvr .. "-l --remote" -- (Optional)
+      end
 
-			return {
-				enhanced_diff_hl = true, -- See ':h diffview-config-enhanced_diff_hl'
-				keymaps = {
-					view = {
-						{ "n", "q", "<cmd>DiffviewClose<CR>", { desc = "Close" } },
-						{ "n", "<Tab>", actions.select_next_entry },
-						{ "n", "<S-Tab>", actions.select_prev_entry },
-						{ "n", "<Leader>a", actions.focus_files },
-						{ "n", "<Leader>e", actions.toggle_files },
-						{ "n", "<Leader>rc", "<cmd>Grc<CR>", { desc = "Git Rebase Continue" } },
-						{ "n", "<Leader>rm", "<cmd>Grm<CR>", { desc = "Git Rebase master/main" } },
-					},
-					file_panel = {
-						{ "n", "q", "<cmd>DiffviewClose<CR>", { desc = "Close" } },
-						{ "n", "c", "<cmd>Gc<CR>", { desc = "Git Commit" } },
-						{ "n", "C", "<cmd>Gcca<CR>", { desc = "Git Commit" } },
-						{ "n", "A", "<cmd>Gca<CR>", { desc = "Git Commit Amend" } },
-						{ "n", "p", "<cmd>Gp<CR>", { desc = "Git Push" } },
-						{ "n", "F", "<cmd>Gpl<CR>", { desc = "Git Push Force (with lease)" } },
-						{ "n", "<Leader>rc", "<cmd>Grc<CR>", { desc = "Git Rebase Continue" } },
-						{ "n", "<Leader>rm", "<cmd>Grm<CR>", { desc = "Git Rebase master/main" } },
-						{ "n", "h", actions.prev_entry({ desc = "Previuos entry" }) },
-					},
-					file_history_panel = {
-						{ "n", "q", "<cmd>DiffviewClose<CR>" },
-						{ "n", "o", actions.focus_entry },
-						{ "n", "O", actions.options },
-						{ "n", "<Leader>ri", rebase_interactive_hash, { desc = "Git Rebase interactive" } },
-					},
-				},
-			}
-		end,
-	},
+      local actions = require("diffview.actions")
+
+      return {
+        enhanced_diff_hl = true, -- See ':h diffview-config-enhanced_diff_hl'
+        keymaps = {
+          view = {
+            { "n", "q", "<cmd>DiffviewClose<CR>", { desc = "Close" } },
+            { "n", "<Tab>", actions.select_next_entry },
+            { "n", "<S-Tab>", actions.select_prev_entry },
+            { "n", "<Leader>a", actions.focus_files },
+            { "n", "<Leader>e", actions.toggle_files },
+            { "n", "<Leader>rc", "<cmd>Grc<CR>", { desc = "Git Rebase Continue" } },
+            { "n", "<Leader>rm", "<cmd>Grm<CR>", { desc = "Git Rebase master/main" } },
+          },
+          file_panel = {
+            { "n", "q", "<cmd>DiffviewClose<CR>", { desc = "Close" } },
+            { "n", "c", "<cmd>Gc<CR>", { desc = "Git Commit" } },
+            { "n", "A", "<cmd>Gca<CR>", { desc = "Git Commit Amend" } },
+            { "n", "p", "<cmd>Gp<CR>", { desc = "Git Push" } },
+            { "n", "F", "<cmd>Gpl<CR>", { desc = "Git Push Force (with lease)" } },
+            { "n", "<Leader>rc", "<cmd>Grc<CR>", { desc = "Git Rebase Continue" } },
+            { "n", "<Leader>rm", "<cmd>Grm<CR>", { desc = "Git Rebase master/main" } },
+            { "n", "h", actions.prev_entry({ desc = "Previuos entry" }) },
+          },
+          file_history_panel = {
+            { "n", "q", "<cmd>DiffviewClose<CR>" },
+            { "n", "o", actions.focus_entry },
+            { "n", "O", actions.options },
+            { "n", "<Leader>ri", rebase_interactive_hash, { desc = "Git Rebase interactive" } },
+          },
+        },
+      }
+    end,
+  },
 }
