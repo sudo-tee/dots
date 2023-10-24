81efc42ccc73a78ec28e63c0ced8cb13374c8111
Francis Belanger <francis.belanger@gmail.com>
2023-10-24 11:29:33 -0400

    feat(nvim) resize call wezterm when pane is at edge

--- config/nvim/lua/plugins/smart-splits.lua
+++ config/nvim/lua/plugins/smart-splits.lua
@@ -1,9 +1,160 @@
+---@return boolean
+local map = vim.keymap.set
+local function at_top_edge()
+  return vim.fn.winnr() == vim.fn.winnr("k")
+end
+
+---@return boolean
+local function at_bottom_edge()
+  return vim.fn.winnr() == vim.fn.winnr("j")
+end
+
+---@return boolean
+local function at_left_edge()
+  return vim.fn.winnr() == vim.fn.winnr("h")
+end
+
+---@return boolean
+local function at_right_edge()
+  return vim.fn.winnr() == vim.fn.winnr("l")
+end
+
+---@param winnr number|nil window ID, defaults to current window
+---@return boolean
+local function is_full_width(winnr)
+  return vim.api.nvim_win_get_width(winnr or 0) == vim.o.columns
+end
+
 return {
   "mrjones2014/smart-splits.nvim",
+  lazy = false,
+  keys = {
+
+    -- resizing splits
+    {
+      "<leader>wr",
+      function()
+        require("smart-splits").start_resize_mode()
+      end,
+      { desc = "Toggle resize window" },
+    },
+
+    -- move between splits
+    {
+      "<C-j>",
+      function()
+        require("smart-splits").move_cursor_down()
+      end,
+    },
+    {
+      "<C-k>",
+      function()
+        require("smart-splits").move_cursor_up()
+      end,
+    },
+    {
+      "<C-l>",
+      function()
+        require("smart-splits").move_cursor_right()
+      end,
+    },
+    {
+      "<C-h>",
+      function()
+        require("smart-splits").move_cursor_left()
+      end,
+    },
+    {
+      "<C-Left>",
+      function()
+        require("smart-splits").move_cursor_left()
+      end,
+    },
+    {
+      "<C-Down>",
+      function()
+        require("smart-splits").move_cursor_down()
+      end,
+    },
+    {
+      "<C-Up>",
+      function()
+        require("smart-splits").move_cursor_up()
+      end,
+    },
+    {
+      "<C-Right>",
+      function()
+        require("smart-splits").move_cursor_right()
+      end,
+    },
+    -- map("n", "<C-A-Right>", function()
+    --   require("smart-splits").resize_right()
+    -- end)
+    -- map("n", "<C-A-Down>", function()
+    --   require("smart-splits").resize_down()
+    -- end)
+    -- map("n", "<C-A-Up>", function()
+    --   require("smart-splits").resize_up()
+    -- end)
+    -- map("n", "<C-A-Left>", function()
+    --   require("smart-splits").resize_left()
+    -- end)
+
+    {
+      "<C-A-h>",
+      function()
+        if at_left_edge() and is_full_width() then
+          local wez = require("lib.wezterm")
+          wez.resize_pane_direction("Left")
+        else
+          require("smart-splits").resize_left()
+        end
+      end,
+    },
+    {
+      "<C-A-j>",
+      function()
+        if at_bottom_edge() and is_full_width() then
+          local wez = require("lib.wezterm")
+          wez.resize_pane_direction("Down")
+        else
+          require("smart-splits").resize_down()
+        end
+      end,
+    },
+    {
+      "<C-A-k>",
+      function()
+        if at_top_edge() and is_full_width() then
+          local wez = require("lib.wezterm")
+          wez.resize_pane_direction("Up")
+        else
+          require("smart-splits").resize_up()
+        end
+      end,
+    },
+    {
+      "<C-A-l>",
+      function()
+        if at_right_edge() and is_full_width() then
+          local wez = require("lib.wezterm")
+          wez.resize_pane_direction("Right")
+        else
+          require("smart-splits").resize_right()
+        end
+      end,
+    },
+  },
   opts = {
+    multiplexer_integration = false,
     ignored_buftypes = { "Neotree" },
-    -- multiplexer_integration = "wezterm",
-    -- at_edge = "wrap",
+    at_edge = function(mux)
+      local utils = require("lib.utils")
+      local wez = require("lib.wezterm")
+
+      wez.activate_pane_direction(utils.first_to_upper(mux.direction))
+    end,
     resize_mode = {
       resize_keys = { "<Left>", "<Down>", "<Up>", "<Right>" },
     },
