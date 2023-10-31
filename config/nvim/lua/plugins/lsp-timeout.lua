a022269d9e7aa40f8389810dc44d94c8700bd29e
Francis Belanger <francis.belanger@gmail.com>
2023-10-31 12:57:57 -0400

    chore(nvim|lsp-timeout): migrate to latest

--- config/nvim/lua/plugins/lsp-timeout.lua
+++ config/nvim/lua/plugins/lsp-timeout.lua
@@ -2,10 +2,10 @@ return {
   "hinell/lsp-timeout.nvim",
   dependencies = { "neovim/nvim-lspconfig" },
   init = function()
-    vim.g["lsp-timeout-config"] = {
+    vim.g.lspTimeoutConfig = {
       stopTimeout = 1000 * 60 * 5, -- ms, timeout before stopping all LSPs
       startTimeout = 1000 * 10, -- ms, timeout before restart
-      silent = false, -- true to suppress notifications
+      silent = true, -- true to suppress notifications
     }
   end,
 }
