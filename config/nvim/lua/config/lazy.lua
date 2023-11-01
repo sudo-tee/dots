75c8b2d6c6fa484d0b62147878686db5606e9268
Francis Belanger <francis.belanger@gmail.com>
2023-11-01 07:19:54 -0400

    feat(nvim|lazy) remove notifications for updates

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -42,7 +42,8 @@ require("lazy").setup({
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
 
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = { enabled = true, notify = false }, -- automatically check for plugin updates
+
   performance = {
     rtp = {
       -- disable some rtp plugins
