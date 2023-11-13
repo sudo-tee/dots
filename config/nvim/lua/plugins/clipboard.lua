01cb8ff61d3e554777cb94cb784018f9b31dce73
Francis Belanger <francis.belanger@gmail.com>
2023-11-13 07:30:25 -0500

    feat(nvim|clipboard): implement built-in OSC52 clipboard

--- config/nvim/lua/plugins/clipboard.lua
+++ config/nvim/lua/plugins/clipboard.lua
@@ -1,16 +1,4 @@
--- Copy to clipboard only when yanking
-vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    if vim.v.event.operator == "y" and vim.v.event.regname == "" then
-      require("osc52").copy_register("+")
-    end
-  end,
-})
-
-vim.keymap.set("n", "<Leader>y", function()
-  local content = vim.fn.getreg('"')
-  vim.fn.setreg("+", content)
-end, { silent = true, desc = "Sync to system clipboard" })
+vim.opt.clipboard = "unnamedplus" -- Sync with system clipboard
 
 vim.g.is_wsl = vim.fn.has("unix")
   and vim.fn.has("wsl")
@@ -32,28 +20,17 @@ if vim.g.is_wsl then
     cache_enabled = 1,
   }
 else
-  local function copy(lines, _)
-    require("osc52").copy(table.concat(lines, "\n"))
-  end
-
-  local function paste()
-    return { vim.fn.split(vim.fn.getreg(""), "\n"), vim.fn.getregtype("") }
-  end
-
   vim.g.clipboard = {
-    name = "osc52",
-    copy = { ["+"] = copy, ["*"] = copy },
-    paste = { ["+"] = paste, ["*"] = paste },
-    cache_enabled = 1,
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.clipboard.osc52").copy,
+      ["*"] = require("vim.clipboard.osc52").copy,
+    },
+    paste = {
+      ["+"] = require("vim.clipboard.osc52").paste,
+      ["*"] = require("vim.clipboard.osc52").paste,
+    },
   }
 end
 
-return {
-  "ojroques/nvim-osc52",
-  event = "VeryLazy",
-  opts = {
-    max_length = 0, -- Maximum length of selection (0 for no limit)
-    silent = true, -- Disable message on successful copy
-    trim = false, -- Trim surrounding whitespaces before copy
-  },
-}
+return {}
