1866c3b5110607f0f103c65406e6893964eeb1e0
francis.belanger@gmail.com <francis.belanger@gmail.com>
2024-01-08 07:05:17 -0500

    fix(bob) fix bob nvim installation

--- apps/bob/_bob.sh
+++ apps/bob/_bob.sh
@@ -1,2 +1,4 @@
-alias bvim="IS_NVIM=true wezterm_run_prog ~/.local/share/bob/nvim-bin/nvim"
-alias nv="IS_NVIM=true wezterm_run_prog ~/.local/share/bob/nvim-bin/nvim"
+path=(
+	~/.local/share/bob/nvim-bin
+	$path
+)
