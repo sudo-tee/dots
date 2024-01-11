c12c7398c0803820b368a4b4468ba6c4ed68943a
Francis Belanger <francis.belanger@gmail.com>
2024-01-11 07:34:13 -0500

    feat(nvim) add marksman lsp

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -18,6 +18,7 @@ return {
           preferGoToSourceDefinition = true,
         },
       },
+      marksman = {},
     },
     -- you can do any additional lsp server setup here
     -- return true if you don't want this server to be setup with lspconfig
