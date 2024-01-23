0f6e3b723973ad3fb8acb23006402e818db13fea
francis.belanger@ubisoft.com <francis.belanger@gmail.com>
2024-01-23 08:38:32 -0500

    fix(nvim): add consistent sort order to project links

--- config/nvim/lua/lib/project-links.lua
+++ config/nvim/lua/lib/project-links.lua
@@ -13,12 +13,13 @@ function M.get_links()
   local u = require("lib.utils")
   local links = {}
   local pos = 1
-  for k, l in pairs(vim.g.project_links or {}) do
-    links[u.rpad(pos .. ". " .. k, 9) .. " | " .. l] = open_url(l)
+
+  for _, link in pairs(vim.g.project_links or {}) do
+    links[u.rpad(pos .. ". " .. link[1], 9) .. " | " .. link[2]] = open_url(link[2])
     pos = pos + 1
   end
 
-  local issue_link = M.get_issue_link()
+  local issue_link = require("lib.jira").get_issue_link()
   if issue_link then
     links[u.rpad(pos .. ". Issue", 9) .. " | " .. issue_link] = open_url(issue_link)
     pos = pos + 1
@@ -26,26 +27,10 @@ function M.get_links()
 
   local mr_url = require("lib.gitlab").get_current_mr_url()
   if mr_url then
-    links[u.rpad(pos .. ". Git MR", 9) .. " | " .. mr_url] = open_url(mr_url)
+    links[u.rpad(pos .. ". MR", 9) .. " | " .. mr_url] = open_url(mr_url)
     pos = pos + 1
   end
   return links
-end
-
-function M.get_issue_link()
-  local cmd_output = vim.fn.system("git branch --show-current"):gsub("\n", "")
-  local jira_issue = M.match_jira_ticket(cmd_output)
-
-  if jira_issue then
-    return vim.g.project_links["Jira"] .. jira_issue
-  end
-
-  return nil
-end
-
-function M.match_jira_ticket(branch_name)
-  local jira_ticket = string.match(branch_name, "%u+%-%d+")
-  return jira_ticket
 end
 
 return M
