0a6c7549b3124dd5551bc5621167540080ac4a8d
Francis Belanger <francis.belanger@gmail.com>
2024-04-30 07:07:25 -0400

    feat(nvim): prioritize local config plugin

--- apps/nvim/config/lua/custom/plugins/nvim-config-local.lua
+++ apps/nvim/config/lua/custom/plugins/nvim-config-local.lua
@@ -1,5 +1,6 @@
 return {
   'klen/nvim-config-local',
+  priority = 999, -- make sure to load this before all the other start plugins
   opts = {
     config_files = { '.nvim.lua', '.nvimrc', '.exrc' },
   },
