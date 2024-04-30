3ccb55c10fa7c911cbb331e2aa0c0851c2f7ff4c
Francis Belanger <francis.belanger@gmail.com>
2024-04-30 07:07:27 -0400

    feat(nvim|eslint): add on_attach function for lint fixes

--- apps/nvim/config/lua/custom/plugins/lsp/servers/eslint.lua
+++ apps/nvim/config/lua/custom/plugins/lsp/servers/eslint.lua
@@ -3,4 +3,12 @@ return {
     -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
     workingDirectories = { mode = 'auto' },
   },
+  on_attach = function(client, bufnr)
+    local u = require('custom.lib.utils')
+    local map = function(mode, keys, func, desc)
+      vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = 'LSP: ' .. desc })
+    end
+
+    map('n', '<leader>cF', u.cmd('EslintFixAll'), '[F]ix all lint issues')
+  end,
 }
