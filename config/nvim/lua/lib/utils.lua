5249f3038b005a1e1772b21ec620460c557b5011
francis.belanger@ubisoft.com <francis.belanger@gmail.com>
2024-04-30 07:07:22 -0400

    wip(nvim): ultimate quit <A-q>

--- config/nvim/lua/lib/utils.lua
+++ config/nvim/lua/lib/utils.lua
@@ -32,6 +32,31 @@ M.close_float_windows = function()
   end)
 end
 
+M.has_float_window = function(filter)
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    if vim.api.nvim_win_is_valid(win) then
+      local config = vim.api.nvim_win_get_config(win)
+
+      if config.relative ~= "" then
+        local win_info = vim.fn.getwininfo(win)
+        local bufnr = win_info[1].bufnr
+        local first_line = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1]
+
+        if not filter or string.find(first_line, filter) then
+          return true
+        end
+      end
+    end
+  end
+  return false
+end
+
+function M.is_buffer_in_split()
+  local total_wins = vim.fn.tabpagewinnr(vim.fn.tabpagenr(), "$")
+
+  return total_wins > 1
+end
+
 M.read_file = function(path)
   local open = io.open
   local file = open(path, "rb") -- r read mode and b binary mode
