4e7c18f1030e3c4e1f78d750fe6c98d888406835
Francis Belanger <francis.belanger@gmail.com>
2024-04-30 07:07:26 -0400

    feat(nvim): replace mini.notify with nvim-notify

--- apps/nvim/config/lua/custom/plugins/mini.lua
+++ apps/nvim/config/lua/custom/plugins/mini.lua
@@ -6,11 +6,9 @@ return { -- Collection of various small independent plugins/modules
     config = function()
       require('custom.plugins.mini.ai')
       require('custom.plugins.mini.comment')
-      -- require('custom.plugins.mini.notify')
       require('custom.plugins.mini.statusline')
       require('custom.plugins.mini.surround')
       require('custom.plugins.mini.tabline')
-      -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
