036702069b6b0051678aef163aeb4e76684641d1
Francis Belanger <francis.belanger@gmail.com>
2024-04-30 07:07:24 -0400

    fix(nvim|color): mute win border color

--- apps/nvim/config/lua/plugins/colorscheme.lua
+++ apps/nvim/config/lua/plugins/colorscheme.lua
@@ -37,7 +37,7 @@ return {
           -- set their background accordingly if you wish to keep them dark and borderless
           LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
           MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-          WinSeparator = { fg = theme.ui.special },
+          WinSeparator = { fg = "#393836" },
         }
       end
       require("kanagawa").setup(opts)
