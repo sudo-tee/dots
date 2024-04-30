fa2583d5e3b22bf938e54ca9b1213aee59fb6494
francis.belanger@ubisoft.com <francis.belanger@gmail.com>
2024-04-30 07:07:23 -0400

    feat(nvim|debugprint) replace debugprint by my own fork of printer.nvim

--- config/nvim/lua/plugins/print.lua
+++ config/nvim/lua/plugins/print.lua
@@ -1,90 +1,94 @@
-local debug_tag = "🧭"
+local DEFAULT_PRINT_TAG = "🧭"
 local function get_print_tag()
-  local print_tag = debug_tag
+  local tag = DEFAULT_PRINT_TAG
   if vim.g.debug_tag then
-    print_tag = debug_tag .. vim.g.debug_tag
+    tag = DEFAULT_PRINT_TAG .. " " .. vim.g.debug_tag
   end
-  return print_tag .. " ❱ "
+  return tag
 end
 
-vim.keymap.set("n", "gP", "<Plug>(printer_print)iw")
+local function is_function(node_type)
+  local valid_types = {
+    function_declaration = true,
+    method_definition = true,
+    function_definition = true,
+    ["function"] = true,
+    arrow_function = true,
+  }
+
+  return valid_types[node_type] ~= nil
+end
+
 local function get_current_function_name()
-  local bufnr = vim.api.nvim_get_current_buf()
+  local node = vim.treesitter.get_node()
 
-  local current_node = vim.treesitter.get_node()
-  if not current_node then
-    return ""
-  end
-
-  local expr = current_node
-  while expr do
-    if
-      expr:type() == "function_declaration"
-      or expr:type() == "method_definition"
-      or expr:type() == "function_definition"
-    then
-      break
+  while node do
+    if is_function(node:type()) then
+      local name = node:field("name")
+      if name and #name >= 1 then
+        return vim.treesitter.get_node_text(name[1], vim.api.nvim_get_current_buf())
+      end
+      return "anonymous"
     end
-    expr = expr:parent()
+    node = node:parent()
   end
-
-  if not expr then
-    return ""
-  end
-
-  local name = expr:field("name")
-
-  if not name or #name < 1 then
-    return "(anonymous)"
-  end
-
-  return vim.treesitter.get_node_text(name[1], bufnr)
 end
 
 return {
-  dir = "~/Projects/_nvim/printer.nvim",
+  "sudo_tee/printer.nvim",
+  -- dir = "~/Projects/_nvim/printer.nvim",
   event = { "VeryLazy" },
+  keys = {
+    { mode = "n", "gP", "<Plug>(printer_print)iw" },
+    {
+      "<localleader>dc",
+      function()
+        vim.fn.setreg([["]], DEFAULT_PRINT_TAG)
+      end,
+      desc = "Copy debug prefix",
+      silent = false,
+    },
+    {
+      "<localleader>px",
+      ":g/" .. DEFAULT_PRINT_TAG .. "/norm va(o$O_d",
+      desc = "Delete prints for buffer",
+      silent = false,
+    },
+    {
+      "<localleader>ps",
+      function()
+        require("telescope.builtin").live_grep({ default_text = DEFAULT_PRINT_TAG })
+      end,
+      desc = "Find all debug print for project",
+      silent = false,
+    },
+  },
   opts = {
-    keymap = "gp", -- Plugin doesn't have any keymaps by default
-    behavior = "insert_below", -- how operator should behave
-    -- "insert_below" will insert the text below the cursor
-    --  "yank" will not insert but instead put text into the default '"' register
+    keymap = "gp",
+    behavior = "insert_below",
     formatters = {
-      -- you can define your formatters for specific filetypes
-      -- by assigning function that takes two strings
-      -- one text modified by 'add_to_inside' function
-      -- second the variable (thing) you want to print out
-      -- see examples in lua/formatters.lua
-      javascript = function(inside, variable)
-        print("🧭 ❱ [print.lua:58] ❱  ❱ javascript:")
-        if variable == "" then
-          return string.format('console.warn("%s")', inside)
+      lua = function(text_inside, text_var)
+        if not text_var then
+          return string.format('print("%s")', text_inside)
         end
-        return string.format('console.warn("%s:", %s)', inside, variable)
-      end,
-      typescript = function(inside, variable)
-        print("🧭 ❱ [print.lua:58] ❱  ❱ typescript:")
-        if variable == "" then
-          return string.format('console.warn("%s")', inside)
-        end
-        return string.format('console.warn("%s:", %s)', inside, variable)
-      end,
-      lua = function(inside, variable)
-        print("🧭 ❱ [print.lua:72] ❱ (anonymous) ❱ lua")
-        if variable == "" then
-          return string.format('print("%s")', inside)
-        end
-        return string.format('print("%s:", vim.inspect(%s))', inside, variable)
+        return string.format('print("%s =" , vim.inspect(%s))', text_inside, text_var)
       end,
     },
     -- function which modifies the text inside string in the print statement, by default it adds the path and line number
     add_to_inside = function(text)
       local fn = get_current_function_name()
-      return string.format("%s[%s:%s] ❱ %s ❱ %s", get_print_tag(), vim.fn.expand("%:t"), vim.fn.line("."), fn, text)
+      local file = vim.fn.expand("%:t")
+      local line = vim.fn.line(".")
+
+      local sections = { get_print_tag(), ("%s:%s"):format(file, line) }
+
+      if fn then
+        table.insert(sections, ("ƒ(%s)"):format(fn))
+      end
+
+      table.insert(sections, ("%s"):format(text or ""))
+
+      return table.concat(sections, " ❱ ")
     end,
-    -- to turn off default behaviour and add nothing
-    -- add_to_inside = function(text)
-    --     return text
-    -- end,
   },
 }
