223353139790139d82e53012c1954c635d5f10fb
Francis Belanger <francis.belanger@gmail.com>
2024-04-30 07:07:27 -0400

    feat(zsh): Update shell aliases and functions

--- config/zsh/aliases.sh
+++ config/zsh/aliases.sh
@@ -6,129 +6,130 @@ alias ...="cd ../.."
 alias .3="cd ../../.."
 alias .4="cd ../../../.."
 
-alias g=git
+g() {
+  git "$@"
+}
 
-alias nvim="IS_NVIM=true ~/.local/share/bob/nvim-bin/nvim"
+function ls() {
+  command ls -lh --color=auto "$@"
+}
 
 alias up="sudo apt update && sudo apt dist-upgrade"
 
 # function aliases
 urldecode() {
-	echo "$1" | python3 -c "import sys; from urllib.parse import unquote; print(unquote(sys.stdin.read()));"
+  echo "$1" | python3 -c "import sys; from urllib.parse import unquote; print(unquote(sys.stdin.read()));"
 }
 
 urlencode() {
-	echo "$1" | python3 -c "import sys; from urllib.parse import quote; print(quote(sys.stdin.read()));"
+  echo "$1" | python3 -c "import sys; from urllib.parse import quote; print(quote(sys.stdin.read()));"
 }
 
 alias urld="urldecode"
 alias urle="urlencode"
 
 mkcd() {
-	mkdir -p $1
-	cd "$1" || exit
+  mkdir -p $1
+  cd "$1" || exit
 }
 
 wtp() {
-	if [ -z "$1" ]; then
-		echo "Usage: wtp [-p][-k] port_number"
-		return 1
-	fi
+  if [ -z "$1" ]; then
+    echo "Usage: wtp [-p][-k] port_number"
+    return 1
+  fi
 
-	local pid_flag
-	if [[ "$1" == "-p" ]]; then
-		shift
-		pid_flag=1
-	fi
+  local pid_flag
+  if [[ "$1" == "-p" ]]; then
+    shift
+    pid_flag=1
+  fi
 
-	local kill_flag
-	if [[ "$1" == "-k" ]]; then
-		if [[ -n "$pid_flag" ]]; then
-			echo "You cannot use the -p flag and the -k flag at the same time"
-			return 1
-		fi
-		shift
-		kill_flag=1
-	fi
+  local kill_flag
+  if [[ "$1" == "-k" ]]; then
+    if [[ -n "$pid_flag" ]]; then
+      echo "You cannot use the -p flag and the -k flag at the same time"
+      return 1
+    fi
+    shift
+    kill_flag=1
+  fi
 
-	if [ -n "$pid_flag" ]; then
-		sudo lsof -i:$1 -t
-	elif [[ -n "$kill_flag" ]]; then
-		kill -9 $(sudo lsof -i:$1 -t)
-	else
-		sudo lsof -i:$1
-	fi
+  if [ -n "$pid_flag" ]; then
+    sudo lsof -i:$1 -t
+  elif [[ -n "$kill_flag" ]]; then
+    kill -9 $(sudo lsof -i:$1 -t)
+  else
+    sudo lsof -i:$1
+  fi
 }
 
 hextorgb() {
-	: "${1/\#/}"
-	((r = 16#${_:0:2}, g = 16#${_:2:2}, b = 16#${_:4:2}))
-	printf '%s\n' "$r $g $b"
+  : "${1/\#/}"
+  ((r = 16#${_:0:2}, g = 16#${_:2:2}, b = 16#${_:4:2}))
+  printf '%s\n' "$r $g $b"
 }
 
 rgbtohex() {
-	printf '#%02x%02x%02x\n' "$1" "$2" "$3"
-}
-
-ucd() {
-	local search_term="${1:-}"
-	local command="${2:-cd}"
-	local dir="$(fdfind "$search_term" "$HOME/Projects" -t d -t l -d 1 --exclude .git | fzf --reverse --select-1)"
-	if [ -n "$dir" ]; then
-		"$command" "$dir"
-	fi
+  printf '#%02x%02x%02x\n' "$1" "$2" "$3"
 }
 
 git_branch() {
-	echo $(git symbolic-ref --short -q HEAD)
+  echo $(git symbolic-ref --short -q HEAD)
 }
 
 git_rebase_current_branch() {
-	# Check if there are unstaged changes
-	if [[ -n $(git status --porcelain) ]]; then
+  # Check if there are unstaged changes
+  if [[ -n $(git status --porcelain) ]]; then
 
-		stash_name="main-rebase-stash-$(date +'%Y%m%d%H%M%S')-$(shuf -i 1000-9999 -n 1)"
+    stash_name="main-rebase-stash-$(date +'%Y%m%d%H%M%S')-$(shuf -i 1000-9999 -n 1)"
 
-		# Prompt the user if they want to stash the changes
-		select choice in "Stash changes" "Abort rebase"; do
-			case $REPLY in
-			1)
-				# Stash the changes
-				git stash
-				break
-				;;
-			2)
-				# Abort the rebase
-				echo "Aborting rebase."
-				return 1
-				;;
-			*)
-				echo "Invalid choice. Please select a valid option."
-				;;
-			esac
-		done </dev/tty
-	fi
+    # Prompt the user if they want to stash the changes
+    select choice in "Stash changes" "Abort rebase"; do
+      case $REPLY in
+        1)
+          # Stash the changes
+          git stash
+          break
+          ;;
+        2)
+          # Abort the rebase
+          echo "Aborting rebase."
+          return 1
+          ;;
+        *)
+          echo "Invalid choice. Please select a valid option."
+          ;;
+      esac
+    done </dev/tty
+  fi
 
-	# Check for existing branches (main or master)
-	if git rev-parse --verify --quiet master; then
-		MAIN_BRANCH="master"
-	else
-		if git rev-parse --verify --quiet main; then
-			MAIN_BRANCH="main"
-		else
-			echo "Neither 'master' nor 'main' branch found."
-			return 1
-		fi
-	fi
+  # Check for existing branches (main or master)
+  if git rev-parse --verify --quiet master; then
+    MAIN_BRANCH="master"
+  else
+    if git rev-parse --verify --quiet main; then
+      MAIN_BRANCH="main"
+    else
+      echo "Neither 'master' nor 'main' branch found."
+      return 1
+    fi
+  fi
 
-	# Fetch the new changes from the upstream repository
-	git fetch origin $MAIN_BRANCH:$MAIN_BRANCH
+  # Fetch the new changes from the upstream repository
+  git fetch origin $MAIN_BRANCH:$MAIN_BRANCH
 
-	# Rebase the current branch on top of the updated upstream branch
-	git rebase $MAIN_BRANCH
+  # Rebase the current branch on top of the updated upstream branch
+  git rebase $MAIN_BRANCH
 
-	# Reapply the named stash if it exists
-	if [[ -n $stash_name ]]; then
-		git stash apply stash@{$stash_name}
-	fi
+  # Reapply the named stash if it exists
+  if [[ -n $stash_name ]]; then
+    git stash apply stash@{$stash_name}
+  fi
+}
+
+## set a custom version of node for nvim which reduce memory consumption
+## @see https://github.com/yioneko/vtsls/issues/136
+nvim() {
+  IS_NVIM=true PATH="$HOME/.local/share/node-v20.12.1-linux-x64-pointer-compression/bin:$PATH" ~/.local/share/bob/nvim-bin/nvim "$@"
 }
