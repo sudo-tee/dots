78758a19284fcdfa52d93e8f027375cbcaa4d209
francis.belanger@ubisoft.com <francis.belanger@gmail.com>
2024-04-30 07:07:22 -0400

    fix(wezterm) fix wezterm install + disable socket

--- apps/wezterm/config/wezterm.lua
+++ apps/wezterm/config/wezterm.lua
@@ -5,7 +5,7 @@ return {
 			-- Override the default path to match the default on the host win32
 			-- filesystem.  This will allow the host to connect into the guest
 			-- container.
-			socket_path = "~/winhome/.local/share/wezterm/sock",
+			--			socket_path = "/run/user/1000/wezterm/sock",
 			-- NTFS permissions will always be "wrong", so skip that check
 			skip_permissions_check = true,
 		},
