08302952da0ba7e2dcd1a33c68b112beafa95938
Francis Belanger <francis.belanger@gmail.com>
2024-04-30 07:07:27 -0400

    feat(nvim): enhance Rust and TypeScript LSP configurations

--- apps/nvim/config/lua/custom/plugins/lsp/servers/vtsls.lua
+++ apps/nvim/config/lua/custom/plugins/lsp/servers/vtsls.lua
@@ -1,8 +1,8 @@
 return {
-  single_file_support = false,
+  single_file_support = true,
   root_dir = require('lspconfig.util').root_pattern('.git'),
-  keys = {},
   on_attach = function(_opts, buff)
+    local u = require('custom.lib.utils')
     local map = function(mode, keys, func, desc)
       vim.keymap.set(mode, keys, func, { buffer = buff, desc = 'LSP: ' .. desc })
     end
@@ -14,9 +14,18 @@ return {
     map('n', '<leader>cA', function()
       require('vtsls').commands.fix_all(0)
     end, 'Fix [a]ll diagnostics')
+
+    map('n', '<leader>cTL', function()
+      require('vtsls').commands.open_tsserver_log()
+    end, 'Open [T]ypescript [L]ogs')
+
+    map('n', '<leader>cr', function()
+      require('vtsls').commands.restart_server()
+    end, '[R]estart TS server')
   end,
   settings = {
     vtsls = {
+      enableMoveToFileCodeAction = true,
       experimental = {
         completion = {
           enableServerSideFuzzyMatch = true,
@@ -24,11 +33,20 @@ return {
       },
     },
     javascript = {
+      updateImportsOnFileMove = 'always',
+      preferGoToSourceDefinition = true,
       format = {
         enabled = false,
       },
     },
     typescript = {
+      updateImportsOnFileMove = 'always',
+      preferGoToSourceDefinition = true,
+      tsserver = {
+        maxTsServerMemory = 8096,
+        -- log = 'verbose',
+        -- nodePath = '/home/francis/.local/share/node-v20.12.1-linux-x64-pointer-compression/bin/node',
+      },
       format = {
         enabled = false,
       },
