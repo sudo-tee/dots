c78f5ac01fdd0afe8c01a9993742b5ce127407c7
Francis Belanger <francis.belanger@gmail.com>
2024-06-05 10:07:17 -0400

    feat(nvim|colorscheme): add dimInactive option

--- apps/nvim/config/lua/custom/plugins/colorscheme.lua
+++ apps/nvim/config/lua/custom/plugins/colorscheme.lua
@@ -6,6 +6,7 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     opts = {
       compile = true,
+      dimInactive = true,
       colors = {
         theme = {
           all = {
