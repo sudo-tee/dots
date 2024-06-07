dd5c12ada1c1bbf01b553f40578215f1104b7c9a
Francis Belanger <francis.belanger@gmail.com>
2024-06-07 10:55:26 -0400

    feat(mini.lua): change event from 'LazyFile' to 'VeryLazy'

--- apps/nvim/config/lua/custom/plugins/mini.lua
+++ apps/nvim/config/lua/custom/plugins/mini.lua
@@ -2,7 +2,7 @@ return { -- Collection of various small independent plugins/modules
   {
     'echasnovski/mini.nvim',
     lazy = true,
-    event = 'LazyFile',
+    event = 'VeryLazy',
     config = function()
       require('custom.plugins.mini.ai').setup()
       require('custom.plugins.mini.statusline').setup()
