ec2dca941b42c71e4716da65906d5920e2719ae9
Francis Belanger <francis.belanger@gmail.com>
2024-06-26 08:33:07 -0400

    feat(nvim): change 'bh' option in GIT_EDITOR command

--- apps/nvim/config/lua/custom/configs/options.lua
+++ apps/nvim/config/lua/custom/configs/options.lua
@@ -110,7 +110,7 @@ vim.cmd.cnoreabbrev('TT', 'Telescope')
 if vim.fn.executable('nvr') == 1 then
   local nvr = 'nvr --servername ' .. vim.v.servername .. ' '
 
-  vim.env.GIT_EDITOR = nvr .. " +'setl bh=delete' --remote-wait"
+  vim.env.GIT_EDITOR = nvr .. " +'setl bh=wipe' --remote-wait"
   vim.env.EDITOR = nvr .. '-l --remote' -- (Optional)
   vim.env.VISUAL = nvr .. '-l --remote' -- (Optional)
 end
