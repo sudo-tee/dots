8f58a5bf92a490cebd1f95ce92d870000a545d90
Francis Belanger <francis.belanger@gmail.com>
2024-07-03 08:07:07 -0400

    feat(nvim|dashboard): configure git-dashboard

--- apps/nvim/config/lua/custom/plugins/dashboard.lua
+++ apps/nvim/config/lua/custom/plugins/dashboard.lua
@@ -1,16 +1,4 @@
-return {
-  'nvimdev/dashboard-nvim',
-  dependencies = { { 'juansalvatore/git-dashboard-nvim', 'nvim-tree/nvim-web-devicons' } },
-  event = 'VimEnter',
-  opts = function()
-    local git_dashboard = require('git-dashboard-nvim').setup({
-      centered = false,
-      top_padding = 10,
-      bottom_padding = 2,
-      show_contributions_count = true,
-    })
-
-    local logo = [[
+local logo = [[
                          ██████████                       
                      ██████████████████                   
                    ██████████████████████                 
@@ -24,6 +12,30 @@ return {
                  ████  ████  ██  ████  ████               
                  ██    ██    ██    ██    ██               
     ]]
+
+return {
+  'nvimdev/dashboard-nvim',
+  dependencies = { { 'juansalvatore/git-dashboard-nvim', 'nvim-tree/nvim-web-devicons' } },
+  event = 'VimEnter',
+  opts = function()
+    local git_dashboard = require('git-dashboard-nvim').setup({
+      fallback_header = logo,
+      use_git_username_as_author = true,
+      centered = false,
+      top_padding = 10,
+      bottom_padding = 2,
+      show_contributions_count = true,
+      empty_square = '◌',
+      filled_squares = { '●', '●', '●', '●', '●', '●', '●' },
+      colors = {
+        days_and_months_labels = '#8FBCBB',
+        empty_square_highlight = '#3B4252',
+        filled_square_highlights = { '#88c0d0', '#88c0d0', '#88c0d0', '#88c0d0', '#88c0d0', '#88c0d0', '#88c0d0' },
+        -- filled_square_highlights = { '#88c0d0', '#9dcad8', '#b1d5df', '#c5dfe7', '#d8eaef', '#ecf4f7', '#ffffff' },
+        branch_highlight = '#88C0D0',
+        dashboard_title = '#88C0D0',
+      },
+    })
 
     local opts = {
       theme = 'doom',
