70b4513ebe39a321c8a900640b3664c96638f03c
Francis Belanger <francis.belanger@gmail.com>
2024-11-13 07:48:54 -0500

    feat: add render-markdown plugin

---
+++ apps/nvim/config/lua/custom/plugins/render-markdown.lua
@@ -0,0 +1,9 @@
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {
+    file_types = { 'markdown', 'Avante' },
+  },
+  ft = { 'markdown', 'Avante' },
+}
