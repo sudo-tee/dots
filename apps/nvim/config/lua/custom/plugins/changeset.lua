2880261daceca27264b4f0fe2486823ffd432069
Francis Belanger <francis.belanger@gmail.com>
2024-11-19 07:46:13 -0500

    feat(nvim|changesets): add Jira link to default text

--- apps/nvim/config/lua/custom/plugins/changeset.lua
+++ apps/nvim/config/lua/custom/plugins/changeset.lua
@@ -1,13 +1,13 @@
 return {
-  'sudo-tee/changesets.nvim',
-  -- dir = '/home/francis/Projects/_nvim/changesets.nvim/',
+  -- 'sudo-tee/changesets.nvim',
+  dir = '/home/francis/Projects/_nvim/changesets.nvim/',
   dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim' },
   ---@module 'changesets'
   ---@type changesets.Opts
   opts = {
+    changed_packages_marker = '✨',
     get_default_text = function()
-      local link = require('custom.lib.jira').format_ticket_as_markdown_link()
-      return link or ''
+      return require('custom.lib.jira').format_ticket_as_markdown_link() or ''
     end,
   },
   keys = {
