af42394374c86399ce4b04a9badc6ed1dcc2819b
Francis Belanger <francis.belanger@gmail.com>
2024-12-03 08:43:56 -0500

    chore(nvim): update blink to new config definition

--- apps/nvim/config/lua/custom/plugins/blink.lua
+++ apps/nvim/config/lua/custom/plugins/blink.lua
@@ -1,64 +1,39 @@
 return {
-  enabled = true,
   'saghen/blink.cmp',
-  -- dir = '~/Projects/_nvim/blink.cmp',
   lazy = false, -- lazy loading handled internally
   -- optional: provides snippets for the snippet source
   dependencies = 'rafamadriz/friendly-snippets',
+
   -- use a release tag to download pre-built binaries
   -- version = 'v0.*',
   -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  build = 'cargo build --release',
+  -- build = 'cargo build --release',
+  -- If you use nix, you can build from source using latest nightly rust with:
+  -- build = 'nix run .#build-plugin',
 
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
-    keymap = {
-      ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-      ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
-      ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
-      ['<CR>'] = { 'accept', 'fallback' },
-      ['<Tab>'] = { 'snippet_forward', 'fallback' },
-      ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
-      ['<Up>'] = { 'select_prev', 'fallback' },
-      ['<Down>'] = { 'select_next', 'fallback' },
-    },
-    highlight = {
-      -- sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- useful for when your theme doesn't support blink.cmp
-      -- will be removed in a future release, assuming themes add support
-      use_nvim_cmp_as_default = true,
-    },
-    -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-    -- adjusts spacing to ensure icons are aligned
-    nerd_font_variant = 'normal',
+    keymap = { preset = 'enter' },
 
-    -- experimental auto-brackets support
-    -- accept = { auto_brackets = { enabled = true } }
+    -- Disables keymaps, completions and signature help for these filetypes
+    blocked_filetypes = {},
 
-    trigger = {
-      completion = {
-        blocked_trigger_characters = { ' ', '\n', '\t', '<', '>', '{', '}' },
-        show_on_insert_on_trigger_character = true,
-      },
-      -- experimental signature help support
-      signature_help = {
-        enabled = true,
-      },
-    },
-    windows = {
-      autocomplete = {
+    completion = {
+      menu = {
         border = 'rounded',
-        -- selection = 'manual',
       },
       documentation = {
         auto_show = true,
-        border = 'rounded',
-        max_width = 100,
+        window = {
+          border = 'rounded',
+        },
       },
-      signature_help = {
+    },
+    signature = {
+      enabled = true,
+      window = {
         border = 'rounded',
-        max_height = 100,
       },
     },
   },
