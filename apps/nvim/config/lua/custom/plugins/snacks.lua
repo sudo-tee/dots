839023b29b7ab92ce0cdc05d5188b777a5d65018
Francis Belanger <francis.belanger@gmail.com>
2024-12-10 14:42:21 -0500

    feat(nvim): configure snacks

--- apps/nvim/config/lua/custom/plugins/snacks.lua
+++ apps/nvim/config/lua/custom/plugins/snacks.lua
@@ -1,12 +1,12 @@
 return {
   'folke/snacks.nvim',
+  event = 'VeryLazy',
   keys = {
     {
       '<leader>up',
       function()
         require('snacks.profiler').toggle()
       end,
-      -- highlights under cursor
     },
     {
       '<leader>uP',
@@ -15,8 +15,32 @@ return {
       end,
       desc = 'Profiler Scratch Bufer',
     },
+    {
+      ']]',
+      desc = 'Next Reference',
+      function()
+        Snacks.words.jump(vim.v.count1)
+      end,
+    },
+    {
+      '[[',
+      desc = 'Prev Reference',
+      function()
+        Snacks.words.jump(-vim.v.count1)
+      end,
+    },
+    {
+      '<leader>un',
+      function()
+        Snacks.notifier.show_history()
+      end,
+      desc = 'Notification History',
+    },
   },
+  ---@type snacks.Config
   opts = {
+    bigfile = { enabled = true },
+    quickfile = { enabled = true },
     styles = {
       notification = {
         wo = { wrap = true },
@@ -25,5 +49,42 @@ return {
     notifier = {
       enabled = true,
     },
+    words = {
+      enabled = true,
+    },
+    scroll = {
+      enabled = true,
+    },
+    debug = {
+      enabled = true,
+    },
   },
+  init = function()
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'VeryLazy',
+      callback = function()
+        -- Setup some globals for debugging (lazy-loaded)
+        _G.dd = function(...)
+          Snacks.debug.inspect(...)
+        end
+        _G.bt = function()
+          Snacks.debug.backtrace()
+        end
+        vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+        -- Create some toggle mappings
+        Snacks.toggle.option('spell', { name = 'Spelling' }):map('<leader>us')
+        Snacks.toggle.option('wrap', { name = 'Wrap' }):map('<leader>uw')
+        Snacks.toggle.option('relativenumber', { name = 'Relative Number' }):map('<leader>uL')
+        Snacks.toggle.diagnostics():map('<leader>ud')
+        Snacks.toggle.line_number():map('<leader>ul')
+        Snacks.toggle
+          .option('conceallevel', { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
+          :map('<leader>uc')
+        Snacks.toggle.treesitter():map('<leader>uT')
+        Snacks.toggle.inlay_hints():map('<leader>uh')
+        Snacks.toggle.indent():map('<leader>ug')
+      end,
+    })
+  end,
 }
