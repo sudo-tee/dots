09d1b89e8cc0758281195ed3b82d47d71dae8fb1
Francis Belanger <francis.belanger@gmail.com>
2024-12-10 14:44:07 -0500

    fix(nvim): fix noice warning

--- apps/nvim/config/lua/custom/plugins/noice.lua
+++ apps/nvim/config/lua/custom/plugins/noice.lua
@@ -24,6 +24,9 @@ return {
         -- ['vim.lsp.util.stylize_markdown'] = true,
         -- ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
       },
+      hover = {
+        enabled = false,
+      },
       signature = {
         enabled = false,
       },
