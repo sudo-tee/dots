5c42c66fcb70b12a4f9da8e5ac6caeaa6563f265
Francis Belanger <francis.belanger@gmail.com>
2024-12-10 14:45:49 -0500

    feat(nvim): add some caching to the status line

---
+++ apps/nvim/config/lua/custom/lib/cache.lua
@@ -0,0 +1,21 @@
+local Cache = {}
+Cache.__index = Cache
+
+function Cache.new(ttl)
+  return setmetatable({
+    ttl = ttl,
+    timestamp = 0,
+    value = nil,
+  }, Cache)
+end
+
+function Cache:get(fetch_fn)
+  local current_time = os.time()
+  if current_time - self.timestamp > self.ttl then
+    self.value = fetch_fn()
+    self.timestamp = current_time
+  end
+  return self.value
+end
+
+return Cache
--- apps/nvim/config/lua/custom/plugins/mini/statusline.lua
+++ apps/nvim/config/lua/custom/plugins/mini/statusline.lua
@@ -11,25 +11,20 @@ M.setup = function()
         local macro = MiniStatusline.macro()
         local git = MiniStatusline.section_git({ trunc_width = 75 })
         local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
-        local diagnostics = MiniStatusline.custom_diagnostics({ trunc_width = 75 })
+        local diagnostics = MiniStatusline.custom_diagnostics()
         local notes = MiniStatusline.section_notes()
         local filename = MiniStatusline.section_filename({ trunc_width = 80 })
         local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 75 })
         local lazy_updates = MiniStatusline.updates()
-        local package_info = require('package-info').get_status()
-
         local location = MiniStatusline.section_location({ trunc_width = 75 })
         local copilot_status = MiniStatusline.copilot_status()
 
         local groups = {
           { hl = 'IncSearch', strings = { search } },
           { hl = mode_hl, strings = { mode } },
-          { hl = 'MiniStatuslineDevinfo', strings = { git, package_info } },
-          { hl = 'MiniStatuslineCustomDiagnosticError', strings = { diagnostics.error } },
-          { hl = 'MiniStatuslineCustomDiagnosticWarn', strings = { diagnostics.warn } },
-          { hl = 'MiniStatuslineCustomDiagnosticInfo', strings = { diagnostics.info } },
-          { hl = 'MiniStatuslineCustomDiagnosticHint', strings = { diagnostics.hint } },
-          { hl = 'MiniStatuslineCustomDiagnosticHint', strings = { notes } },
+          { hl = 'MiniStatuslineDevinfo', strings = { git } },
+          unpack(diagnostics) or '',
+          { hl = 'MiniStatuslineCustomNotes', strings = { notes } },
           '%<', -- Mark general truncate point
           { hl = 'MiniStatuslineFilename', strings = { filename } },
           '%=', -- End left alignment
@@ -46,7 +41,7 @@ M.setup = function()
   })
 
   ---@diagnostic disable-next-line: duplicate-set-field
-  MiniStatusline.section_filename = function(args)
+  MiniStatusline.section_filename = function()
     -- In terminal always use plain name
     if vim.bo.buftype == 'terminal' then
       return '%t'
@@ -73,21 +68,36 @@ M.setup = function()
     return status
   end
 
-  -- little hackish solution to dislay diagnostics icons and colors
+  local diagnostic_levels = {
+    { name = 'ERROR', sign = ' ' },
+    { name = 'WARN', sign = ' ' },
+    { name = 'INFO', sign = '󰋼 ' },
+    { name = 'HINT', sign = '󰌵 ' },
+  }
 
-  MiniStatusline.custom_diagnostics = function(opts)
-    local diagnostics = MiniStatusline.section_diagnostics(opts)
-    local icons = { E = ' ', W = ' ', I = '󰋼 ', H = '󰌵 ' }
-    local sections = { error = 'E', warn = 'W', info = 'I', hint = 'H' }
+  MiniStatusline.custom_diagnostics = (function()
+    local utils = require('custom.lib.utils')
+    local cache = {}
 
-    local result = {}
-    for key, symbol in pairs(sections) do
-      local count = diagnostics:match(symbol .. '%d+') or ''
-      result[key] = count:gsub(symbol, icons[symbol])
+    -- Update cache when diagnostics change
+    vim.api.nvim_create_autocmd('DiagnosticChanged', {
+      group = utils.augroup('custom_statusline_diagnostics'),
+      callback = function()
+        cache = {}
+        for _, level in ipairs(diagnostic_levels) do
+          local n = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity[level.name] })
+          if n > 0 then
+            table.insert(cache, { hl = 'MiniStatuslineCustomDiagnostic' .. level.name, strings = { level.sign .. n } })
+          end
+        end
+      end,
+    })
+
+    -- Return cached result
+    return function()
+      return cache
     end
-
-    return result
-  end
+  end)()
 
   MiniStatusline.macro = function(_)
     local reg = vim.fn.reg_recording()
@@ -96,13 +106,8 @@ M.setup = function()
     return macro
   end
 
-  MiniStatusline.updates = function(_)
-    if require('lazy.status').has_updates() then
-      return require('lazy.status').updates()
-    end
-  end
   ---@diagnostic disable-next-line: duplicate-set-field
-  MiniStatusline.section_location = function(_)
+  MiniStatusline.section_location = function()
     -- Use virtual column number to allow update when past last column
     return '%2l:%-2v'
   end
@@ -110,31 +115,39 @@ M.setup = function()
   local note_exists_cache = {}
   local project_name
 
+  ---@TODO Add a Custom auto_cmmand when note is created
   MiniStatusline.section_notes = function()
     local utils = require('custom.lib.utils')
     local notes_path = vim.g.notes_dir
-    project_name = project_name or vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
+    project_name = project_name or vim.uv.cwd():match('[^/\\]+$') or ''
 
-    local filename = vim.fn.expand('%:t')
-    local file_path = vim.fn.expand('%:p:h')
-    if filename == '' then
+    local bufname = vim.api.nvim_buf_get_name(0)
+    local filename = bufname:match('[^/\\]+$')
+    if not filename or filename == '' then
       return ''
     end
 
+    -- Early return if cached result exists
     if note_exists_cache[filename] ~= nil then
       return note_exists_cache[filename]
     end
 
+    local file_path = bufname:match('^(.*)/[^/]*$') or ''
     local note_filename = string.format('%s-%s.md', filename, utils.string_hash(file_path))
     local path = utils.path_join(notes_path, project_name, note_filename)
 
-    if vim.fn.filereadable(path) == 1 then
-      note_exists_cache[filename] = ''
-      return ''
-    else
-      note_exists_cache[filename] = ''
-      return ''
-    end
+    -- Cache and return result
+    local result = vim.fn.filereadable(path) == 1 and '' or ''
+    note_exists_cache[filename] = result
+    return result
+  end
+
+  local updates_cache = require('custom.lib.cache').new(300) -- 15 minutes cache
+  MiniStatusline.updates = function(_)
+    return updates_cache:get(function()
+      local lazy_status = require('lazy.status')
+      return lazy_status.has_updates() and lazy_status.updates() or nil
+    end)
   end
 end
 
