d1bfa37af8642ada5e99977d570eab2f796bc858
Francis Belanger <francis.belanger@gmail.com>
2024-12-11 08:19:01 -0500

    feat(nvim): fix blink build

--- apps/nvim/config/lua/custom/plugins/blink.lua
+++ apps/nvim/config/lua/custom/plugins/blink.lua
@@ -7,7 +7,7 @@ return {
   -- use a release tag to download pre-built binaries
   -- version = 'v0.*',
   -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  -- build = 'cargo build --release',
+  build = 'cargo build --release',
   -- If you use nix, you can build from source using latest nightly rust with:
   -- build = 'nix run .#build-plugin',
 
