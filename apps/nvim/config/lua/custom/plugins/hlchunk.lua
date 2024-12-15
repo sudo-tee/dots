2359fe2785bd761d63e1983847b86d9516dda442
Francis Belanger <francis.belanger@gmail.com>
2024-12-15 15:39:19 -0500

    feat: add hlchunk

---
+++ apps/nvim/config/lua/custom/plugins/hlchunk.lua
@@ -0,0 +1,26 @@
+return {
+  'shellRaining/hlchunk.nvim',
+  event = { 'UIEnter' },
+  config = function()
+    require('hlchunk').setup({
+      chunk = {
+        enable = true,
+        use_treesitter = true,
+        chars = {
+          horizontal_line = '─',
+          vertical_line = '│',
+          left_top = '╭',
+          left_bottom = '╰',
+          right_arrow = '>',
+        },
+      },
+      blank = {
+        enable = false,
+      },
+      line_num = {
+        enable = true,
+        use_treesitter = true,
+      },
+    })
+  end,
+}
