d7927c6b2d7a70ddb5806e9def5964940adb9c73
Francis Belanger <francis.belanger@gmail.com>
2025-02-20 07:00:05 -0500

    chore(nvim): update blink

--- apps/nvim/config/lua/custom/plugins/blink.lua
+++ apps/nvim/config/lua/custom/plugins/blink.lua
@@ -21,9 +21,9 @@ return {
   dependencies = { 'rafamadriz/friendly-snippets' },
 
   -- use a release tag to download pre-built binaries
-  -- version = 'v0.*',
+  version = 'v0.*',
   -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  build = 'cargo build --release',
+  -- build = 'cargo build --release',
   -- If you use nix, you can build from source using latest nightly rust with:
   -- build = 'nix run .#build-plugin',
 
@@ -73,8 +73,10 @@ return {
       },
     },
     sources = {
-      cmdline = {},
       default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
+    cmdline = {
+      enabled = false,
     },
   },
   opts_extend = { 'sources.default' },
