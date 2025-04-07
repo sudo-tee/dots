fd29c737af5a42de08345f034cf3e10059d06001
Francis Belanger <francis.belanger@gmail.com>
2025-04-07 07:33:50 -0400

    feat(nvim): remove uneeded treewalker plugin

--- apps/nvim/config/lua/custom/plugins/treewalker.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'aaronik/treewalker.nvim',
-  keys = {
-    { '<leader>w<Down>', '<cmd>Treewalker Down<cr>', silent = true, desc = 'Down (Treewalker)' },
-    { '<leader>w<Up>', '<cmd>Treewalker Up<cr>', silent = true, desc = 'Up (Treewalker)' },
-    { '<leader>w<Left>', '<cmd>Treewalker Left<cr>', silent = true, desc = 'Left (Treewalker)' },
-    { '<leader>w<Right>', '<cmd>Treewalker Right<cr>', silent = true, desc = 'Right (Treewalker)' },
-  },
-}
