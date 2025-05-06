43af6a5f9c94b671973a2e9845b35d533e025557
Francis Belanger <francis.belanger@gmail.com>
2025-05-06 06:53:45 -0400

    feat(nvim|blink): upgrade to a 1.* release

--- apps/nvim/config/lua/custom/plugins/blink.lua
+++ apps/nvim/config/lua/custom/plugins/blink.lua
@@ -21,7 +21,7 @@ return {
   dependencies = { 'rafamadriz/friendly-snippets' },
 
   -- use a release tag to download pre-built binaries
-  version = 'v0.*',
+  version = 'v1.*',
   -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
   -- build = 'cargo build --release',
   -- If you use nix, you can build from source using latest nightly rust with:
@@ -54,6 +54,7 @@ return {
           preselect = true,
           auto_insert = false,
         },
+        max_items = 15,
       },
       menu = {
         border = 'rounded',
