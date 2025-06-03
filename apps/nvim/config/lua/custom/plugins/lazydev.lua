05ef1b996a09c93ee5c34bb4ad2f5d50d3c97f53
Francis Belanger <francis.belanger@gmail.com>
2025-06-03 07:28:25 -0400

    feat(nvim): add lazydev to blink

--- apps/nvim/config/lua/custom/plugins/lazydev.lua
+++ apps/nvim/config/lua/custom/plugins/lazydev.lua
@@ -24,6 +24,23 @@ return {
     },
   },
   { 'Bilal2453/luvit-meta', lazy = true }, -- optional `vim.uv` typings
+  { -- optional blink completion source for require statements and module annotations
+    'saghen/blink.cmp',
+    opts = {
+      sources = {
+        -- add lazydev to your completion providers
+        default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+        providers = {
+          lazydev = {
+            name = 'LazyDev',
+            module = 'lazydev.integrations.blink',
+            -- make lazydev completions top priority (see `:h blink.cmp`)
+            score_offset = 100,
+          },
+        },
+      },
+    },
+  },
   -- { -- optional completion source for require statements and module annotations
   --   'hrsh7th/nvim-cmp',
   --   opts = function(_, opts)
