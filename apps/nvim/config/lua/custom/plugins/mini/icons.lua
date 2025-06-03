bd59ca291cce1b77786039c89e43a96305dcf8b7
Francis Belanger <francis.belanger@gmail.com>
2025-06-03 07:29:49 -0400

    feat(nvim): move mini icons in own file

---
+++ apps/nvim/config/lua/custom/plugins/mini/icons.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+M.setup = function()
+  local mini_icons = require("mini.icons")
+  mini_icons.setup({
+    extension = {
+      ['spec.ts'] = { glyph = '󰤒', hl = 'MiniIconsGreen' },
+      ['test.ts'] = { glyph = '󰤒', hl = 'MiniIconsGreen' },
+    },
+  })
+  if mini_icons.mock_nvim_web_devicons then
+    mini_icons.mock_nvim_web_devicons()
+  end
+end
+
+return M
