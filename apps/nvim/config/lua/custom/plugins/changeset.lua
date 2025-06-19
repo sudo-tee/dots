c82988dcec014aa406e745d113028510f10c7d6c
Francis Belanger <francis.belanger@gmail.com>
2025-06-19 10:13:17 -0400

    feat(nvim): update changeset config for last version

--- apps/nvim/config/lua/custom/plugins/changeset.lua
+++ apps/nvim/config/lua/custom/plugins/changeset.lua
@@ -2,13 +2,17 @@ return {
   -- enabled = false,
   -- 'sudo-tee/changesets.nvim',
   dir = '/home/francis/Projects/_nvim/changesets.nvim/',
-  -- dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim' },
   ---@module 'changesets'
   ---@type changesets.Opts
   opts = {
     changed_packages_marker = '✨',
     get_default_text = function()
       return require('custom.lib.jira').format_ticket_as_markdown_link() or ''
+    end,
+    filter_packages = function(packages)
+      return vim.tbl_filter(function(pkg)
+        return not pkg.path:match('/e2e%-tests?/')
+      end, packages)
     end,
   },
   keys = {
