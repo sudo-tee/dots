d09eb4f50868ab3027ad2d765e5c66ac643eeed2
Francis Belanger <francis.belanger@gmail.com>
2025-09-28 13:40:31 -0400

    feat: update compressed pointer node to 22.1

--- apps/nvim/_nvim.sh
+++ apps/nvim/_nvim.sh
@@ -1,5 +1,5 @@
 ## set a custom version of node for nvim which reduce memory consumption
 ## @see https://github.com/yioneko/vtsls/issues/136
 nvim() {
-  IS_NVIM=true PATH="$HOME/.local/share/node-v20.12.1-linux-x64-pointer-compression/bin:$PATH" ~/.local/share/bob/nvim-bin/nvim "$@"
+  IS_NVIM=true PATH="$HOME/.local/share/node-v22.1.0-linux-x64-pointer-compression/bin:$PATH" ~/.local/share/bob/nvim-bin/nvim "$@"
 }
