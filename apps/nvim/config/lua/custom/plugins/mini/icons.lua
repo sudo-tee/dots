4d9c2935fd6f690ab09c1bd37e02f2940fee2369
Francis Belanger <francis.belanger@gmail.com>
2025-10-06 16:04:40 -0400

    feat(nvim): add mote test icons

--- apps/nvim/config/lua/custom/plugins/mini/icons.lua
+++ apps/nvim/config/lua/custom/plugins/mini/icons.lua
@@ -1,11 +1,13 @@
 local M = {}
 
 M.setup = function()
-  local mini_icons = require("mini.icons")
+  local mini_icons = require('mini.icons')
   mini_icons.setup({
     extension = {
       ['spec.ts'] = { glyph = '󰤒', hl = 'MiniIconsGreen' },
       ['test.ts'] = { glyph = '󰤒', hl = 'MiniIconsGreen' },
+      ['spec.tsx'] = { glyph = '󰤒', hl = 'MiniIconsGreen' },
+      ['test.tsx'] = { glyph = '󰤒', hl = 'MiniIconsGreen' },
     },
   })
   if mini_icons.mock_nvim_web_devicons then
